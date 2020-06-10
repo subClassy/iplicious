6627f5b1bca483cafef7829f775b37a5c3da9097
Himank Goel <himankgoel03@gmail.com>
2020-06-10 19:48:54 +0530

    Starter Code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
 import React from "react";
-import { Button } from "antd";
+import { Layout, Menu, Breadcrumb } from "antd";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import "./App.css";
 
-const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+const iconPath = process.env.PUBLIC_URL + "/assets/";
+
+const { Header, Content, Sider } = Layout;
+const { SubMenu } = Menu;
+
+class App extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  onCollapse = (collapsed) => {
+    this.setState({ collapsed });
+  };
+
+  render() {
+    return (
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <div className="logo">
+            <img src={`${iconPath}full-logo.svg`} alt="Logo" />
+          </div>
+          <Menu defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Batting
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Bowling
+            </Menu.Item>
+            <Menu.Item key="3" icon={<FileOutlined />}>
+              Venue
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              Bill is a cat.
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
+}
 
 export default App;
